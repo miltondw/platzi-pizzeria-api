06841cc9b001f87b6a0a94fa598419bb69f2a4e9
miltondw <estradamilton2001@gmail.com>
2023-07-04 11:10:03 -0500

    Custom result with @Query creating a summary of the order

--- src/main/java/com/platzi/pizza/service/OrderService.java
+++ src/main/java/com/platzi/pizza/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.platzi.pizza.service;
 
 import com.platzi.pizza.persistence.entity.OrderEntity;
+import com.platzi.pizza.persistence.projection.OrderSummary;
 import com.platzi.pizza.persistence.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,5 +37,8 @@ public class OrderService {
     }
     public List<OrderEntity> getCustomerOrders(String id){
         return this.orderRepository.findCustomerOrders(id);
+    }
+    public OrderSummary getSummary(int orderId) {
+        return this.orderRepository.findSummary(orderId);
     }
 }
