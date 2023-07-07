bcf389b9eee31756a60f1848a156f8b5373e17c7
miltondw <estradamilton2001@gmail.com>
2023-07-07 10:51:16 -0500

    Disable CSRF protection

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,8 +16,8 @@ public class SecurityConfig {
                             customizeRequests
                                     .anyRequest()
                                     .authenticated();
-                        }
-                ).httpBasic(Customizer.withDefaults());
+                        }).httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
